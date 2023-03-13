4ab9a92f7b3c8805fb20fbba20fd4f9cebcc9ed4
adamfadrian <adamfadrian12@gmail.com>
2023-03-13 14:14:23 +0700

    add login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 
 
@@ -8,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Cookies, useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
+import { logout } from '../store/features/userSlice'
+import { useDispatch } from 'react-redux'
 
 const Home = () => {
+const [cookies, setCookies] = useCookies(['userToken'])
+const navigate = useNavigate()
+// const auth = useSelector((state: { auth: AuthState }) => state.auth)
+  const dispatch = useDispatch()
+
+useEffect(() => {
+    if (!cookies.userToken) {
+      dispatch(logout());
+    }
+  }, [cookies.userToken, dispatch]);
     return (
-        <div>
-            home
+        <div className='flex justify-center'>
+            <button className='btn btn-wide' onClick={() => navigate('/')}>back to login</button>
         </div>
     )
 }
