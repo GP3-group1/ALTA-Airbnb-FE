faa1edc41af348dae010ccdc437dffe9f33c0098
adamfadrian <adamfadrian12@gmail.com>
2023-03-14 15:25:44 +0700

    handle logout

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Select, Option } from "@material-tailwind/react";
 
-
+import Swal from 'sweetalert2';
 import { AiOutlineHome } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import { BiSearch } from 'react-icons/bi'
 import { useNavigate } from 'react-router'
+import { useCookies } from 'react-cookie';
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/features/userSlice';
 
 
 
 
 const Navbar = () => {
     const navigate = useNavigate()
-
+    const [cookies, setCookie, removeCookie] = useCookies(["userToken"]);
     const [isOpen, setIsOpen] = useState(false);
     const [scroll , setScroll ] = useState<any>()
+    const dispatch = useDispatch()
 
     const goProfile = () => {
         navigate('/Account')
@@ -24,6 +28,42 @@ const Navbar = () => {
         navigate('/home')
     }
 
+
+const handleLogout = useCallback(() => {
+    Swal.fire({
+      title: "Are you sure?",
+      // text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Yes",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          text: "Logout successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        dispatch(logout());
+        removeCookie("userToken");
+        navigate("/");
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!cookies.userToken) {
+      dispatch(logout());
+      navigate('/')
+    }
+  }, [cookies.userToken, dispatch,navigate]);
+
+
+
     const scrolly = addEventListener('scroll' , function() {
         setScroll(window.scrollY)
         console.log(window.scrollY)
@@ -32,7 +72,7 @@ const Navbar = () => {
 
     window.scrollY
     return (
-        <div className=' sticky top-0 '>
+        <div className=' sticky top-0 z-50'>
 
             {screen.width > 767
                 ? <div className={`w-full h-20 flex flex-row justify-around items-center   ${scroll > 50 ? 'bg-[#4397fb]' : 'bg-white'} duration-500  shadow-md`}>
@@ -54,7 +94,7 @@ const Navbar = () => {
                             <div className="absolute right-0 z-10 w-full mt-2 origin-top-right rounded-md shadow-lg bg-white ring-1 ring-gray-200" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                                 <div className="py-1" role="none">
                                     <a href="#" className="block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" onClick={goProfile}>Account</a>
-                                    <a href="#" className="block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" >Log-out</a>
+                                    <a href="#" onClick={handleLogout} className="block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem" >Log-out</a>
                                 </div>
                             </div>
                         )}
